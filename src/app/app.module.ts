8d331d8f70a9b57ad61b6433986cec8712cd1738
biel12321 <gabriellw.grb@gmail.com>
2019-05-15 21:38:28 -0300

    crud finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,20 @@
+import { HttpClientModule } from '@angular/common/http';
+import { CategoriasPesquisaComponent } from './categorias/categorias-pesquisa/categorias-pesquisa.component';
+import { CategoriasCadastroComponent } from './categorias/categorias-cadastro/categorias-cadastro.component';
 import { CategoriasModule } from './categorias/categorias.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { Routes, RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ConfirmationService } from 'primeng/api';
+
+const rotas: Routes = [
+  {path: 'categorias', component: CategoriasPesquisaComponent},
+  {path: 'categorias/novo', component: CategoriasCadastroComponent},
+  {path: 'categorias/:id', component: CategoriasCadastroComponent},
+];
 
 @NgModule({
   declarations: [
@@ -13,7 +23,9 @@ import { ConfirmationService } from 'primeng/api';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    CategoriasModule
+    CategoriasModule,
+    HttpClientModule,
+    RouterModule.forRoot(rotas) //forRoot é usado para indicar qual a tela ele ira.
   ],
   providers: [ConfirmationService],
   bootstrap: [AppComponent]
